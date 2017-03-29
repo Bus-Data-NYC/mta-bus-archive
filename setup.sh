7e857992b631ac8b3b537125f71d0e4cd61be0dd
fitnr <fitnr@fakeisthenewreal>
2017-03-29 15:34:49 -0400

    lint/

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 # setup.sh db_name
 
 if [ $# -lt 1 ]; then
-        echo Usage: `basename $0` db_name
-        exit 1
+    echo Usage: "$(basename $0)" db_name
+    exit 1
 fi
 
 read -p 'Are you sure?  All existing positions data will be deleted.  [y/N]: ' -r
@@ -13,6 +14,4 @@ then
 fi
 
 DB_NAME=$1
-
 mysql $DB_NAME < archive_schema.sql
-
