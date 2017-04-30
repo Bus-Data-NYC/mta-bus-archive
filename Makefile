629567da43fba63de30733b36852e67f1f496b13
fitnr <fitnr@fakeisthenewreal>
2017-04-30 16:40:14 -0400

    add postgres support (indeed make it default)

--- Makefile
+++ Makefile
@@ -4,11 +4,19 @@ MYSQLFLAGS =
 DATABASE = nycbus
 MYSQL = mysql $(DATABASE) $(MYSQLFLAGS)
 
+PSQLFLAGS = 
+PSQL = psql $(DATABASE) $(PSQLFLAGS)
+
 ARCHIVE = http://data.mytransit.nyc.s3.amazonaws.com/bus_time
 
 DATE = 20161101
 
-.PHONY: mysql mysql-%
+.PHONY: mysql mysql-% psql psql-% psql_init mysql_init
+
+psql: psql-$(DATE)
+
+psql-%: csv/bus_time_%.csv
+	$(PSQL) -c "\copy positions FROM '$<' DELIMITER ',' HEADER NULL '\N' CSV"
 
 mysql: mysql-$(DATE)
 
@@ -18,6 +26,8 @@ mysql-%: csv/bus_time_%.csv
 		FIELDS TERMINATED BY ',' \
 		LINES TERMINATED BY '\r\n' \
 		IGNORE 1 LINES"
+
+.PRECIOUS: csv/bus_time_%.csv.csv
 
 csv/%.csv: xz/%.csv.xz | csv
 	@rm -f $@
@@ -29,8 +39,11 @@ xz/bus_time_%.csv.xz: | xz
 	$(eval MONTH=$(shell echo $* | sed 's/.\{4\}\(.\{2\}\).*/\1/'))
 	curl -o $@ $(ARCHIVE)/$(YEAR)/$(YEAR)-$(MONTH)/$(@F)
 
-init: sql/archive_schema.sql
+mysql_init: sql/archive_schema.mysql
 	$(MYSQL) < $<
+
+psql_init: sql/archive_schema.sql
+	$(PSQL) -f $<
 
 clean:
 	rm -rf xz csv
--- sql/archive_schema.sql
+++ sql/archive_schema.sql
@@ -1,19 +1,21 @@
-# archive_schema.sql
-
+-- archive_schema.sql
 DROP TABLE IF EXISTS positions;
 CREATE TABLE positions (
-	timestamp_utc datetime NOT NULL,
-	vehicle_id smallint(4) ZEROFILL NOT NULL,
-	latitude decimal(8, 6) NOT NULL,
-	longitude decimal(9, 6) NOT NULL,
-	bearing decimal(5, 2) NOT NULL,
-	progress tinyint(1) NOT NULL,
-	service_date date NOT NULL,
-	trip_id VARCHAR(255) NOT NULL,
-	block_assigned tinyint(1) NOT NULL,
-	next_stop_id int(6),
-	dist_along_route decimal(8, 2),
-	dist_from_stop decimal(8, 2),
-	PRIMARY KEY (timestamp_utc, vehicle_id)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
+    timestamp_utc TIMESTAMP NOT NULL,
+    vehicle_id INTEGER NOT NULL,
+    latitude NUMERIC(8, 6) NOT NULL,
+    longitude NUMERIC(9, 6) NOT NULL,
+    bearing NUMERIC(5, 2) NOT NULL,
+    progress INTEGER NOT NULL,
+    service_date DATE NOT NULL,
+    trip_id TEXT NOT NULL,
+    block_assigned BOOLEAN NOT NULL,
+    next_stop_id INTEGER,
+    dist_along_route NUMERIC(8, 2),
+    dist_from_stop NUMERIC(8, 2),
+    CONSTRAINT position_time_bus PRIMARY KEY (timestamp_utc, vehicle_id)
+);
+CREATE INDEX pos_vid ON positions (vehicle_id);
+CREATE INDEX pos_sdate ON positions (service_date);
+CREATE INDEX pos_trip_id ON positions (trip_id);
+CREATE INDEX pos_time ON positions (timestamp_utc);
